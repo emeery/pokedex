69e536d479ff4202f1ebacb88e080f327c5659d4
emeery <gerardoemery@live.com.mx>
2019-10-02 17:37:27 -0500

    selection

--- src/app/components/pokelista/pokelista.component.ts
+++ src/app/components/pokelista/pokelista.component.ts
@@ -3,6 +3,7 @@ import { Pokemon } from '../models/pokemon.model';
 import { PokeService } from './poke.service';
 import { Subscription } from 'rxjs';
 import { MatPaginator, MatTableDataSource} from '@angular/material';
+import { SelectionModel } from '@angular/cdk/collections';
 @Component({
   selector: 'app-pokelista',
   templateUrl: './pokelista.component.html',
@@ -11,8 +12,11 @@ import { MatPaginator, MatTableDataSource} from '@angular/material';
 
 export class PokelistaComponent implements OnInit, OnDestroy {
   dataSource = new MatTableDataSource<Pokemon>(); // arreglo de Pokemon
-  cols: string[] = ['id', 'nombre', 'tipo', 'imagen']; // columnas tabla lista
+  cols: string[] = ['select','id', 'nombre', 'tipo', 'imagen']; // columnas tabla lista
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator; // paginacion Angular Material
+  selection = new SelectionModel<Pokemon>(true, []);
+  selectedColumnas: Array<{}> = [];
+  checked = false;
   pokemonPorPagina = 5;
   pokemon: Pokemon[]; // arreglo de tipo Pokemon
   subs: Subscription; // subscripción al observable Pokemon
@@ -23,12 +27,41 @@ export class PokelistaComponent implements OnInit, OnDestroy {
     this.dataSource.paginator = this.paginator;
     this.getPokemon();
   }
-  getPokemon() {
+  getPokemon() { // lista pokemon
     this.pokemon = this.pokeServicio.getPokemon();
     this.dataSource.data = this.pokemon;
     this.subs = this.pokeServicio.pokemonObservable
     .subscribe((poke: Pokemon[]) => {
       this.pokemon = poke;
+    });
+  }
+  makeFiltro(v: string) {
+    this.dataSource.filter = v.trim().toLocaleLowerCase();
+  }
+  // Selecciona todas las filas si no están todas seleccionadas; de lo contrario, selección clara
+  masterToggle(ref) {
+    // console.log(ref.checked); // true o false
+    if (this.isSomeSelected()) {
+      this.selection.clear();
+      ref.checked = false;
+    } else {
+      this.isAllSelected() ?
+      this.selection.clear() : this.dataSource.data.forEach(row => this.selection.select(row));
+    }
+  }
+  // Si el número de elementos seleccionados coincide con el número total de filas.
+  isAllSelected() {
+    const numSeleccion = this.selection.selected.length;
+    const numColumnas = this.dataSource.data.length; // columnas
+    return numSeleccion === numColumnas;
+  }
+  isSomeSelected() {
+    return this.selection.selected.length > 0;
+  }
+  store() {
+    setTimeout(() => {
+      this.selectedColumnas = this.selection.selected;
+      console.log('columnasSeleccionadas: ', this.selectedColumnas);
     });
   }
   ngOnDestroy() {
